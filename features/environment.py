e280b1171468ad556e07ea4c352efadd2fb9de26
masonou-vegas <masonou@hotmail.com>
2025-07-20 11:22:02 -0700

    browserstack assignment

--- features/environment.py
+++ features/environment.py
@@ -1,43 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from app.application import Application
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from app.application import Application
 
-
-
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
     driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
-    #options = Options()
-    #options.add_argument("--headless=new")
-    #context.driver = webdriver.Chrome(options=options)
-    #context.driver = webdriver.Chrome(service=service)
+    #context.driver = webdriver.Chrome (service=service)
+
+    ### SAFARI ###
+    # context.driver = webdriver.Firefox()
+    # context.driver = webdriver.Safari()
 
     ### HEADLESS MODE ####
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    context.driver = webdriver.Chrome(options=options)
-    #context.driver = webdriver.Firefox()
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # context.driver = webdriver.Chrome(
+    #     options=options
+    # )
+
+    ### BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user ='masonmcderp_3WJDjn'
+    bs_key = 'zwybyEkxyBLrD8ZWx1tM'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os" : "Windows",
+        "osVersion" : "11",
+        'browserName': 'Edge',
+        'sessionName': scenario_name,
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(6)
+    context.driver.wait = WebDriverWait(context.driver, timeout=10)
     context.app = Application(context.driver)
-
-    context.driver.save_screenshot("screenshot.png")
-    print(context.driver.page_source)
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
@@ -47,7 +58,6 @@ def before_step(context, step):
 def after_step(context, step):
     if step.status == 'failed':
         print('\nStep failed: ', step)
-
 
 
 def after_scenario(context, feature):
